6859bf97073d4d9475f7b4bbed792ab1c2145deb
Joseph Maifeld <josephmaifeld@gmail.com>
2022-02-17 10:26:40 -0600

    functions-deconstuct

---
+++ main.js
@@ -0,0 +1,52 @@
+//Repotting a plant
+
+const pick = () => {	
+	console.log(`Choose new pot slightly larger then current pot`)
+}
+
+const moisten = () => {
+	console.log(`Lightly water plant to prevent root breakage during repotting`)
+}
+
+const remove = () => {
+	console.log(`Slowly work plant loose from old pot`)
+}
+
+const prep = () => {
+	console.log(`Trim away dead roots and loosen root ball`)
+}
+
+const soil = () => {
+	console.log(`Layer in well draining soil into new pot`)
+}
+
+const repot = () => {
+	console.log(`Place plant in new pot and fill with soil to top off`)
+}
+
+const water = () => {
+	console.log(`Thoroughly water plant`)
+}
+
+const place = () => {
+	console.log(`Choose new placement for plant according to lighting and temp needs`)
+}
+
+pick()
+moisten()
+remove()
+prep()
+soil()
+repot()
+water()
+place()
+
+
+//function flow
+
+// const add =(num1, num2)=>{
+// 	return num1+num2
+// }
+// const invoke=add(3,5)
+// const multi=multiply(invoke)
+// console.log(multi)
